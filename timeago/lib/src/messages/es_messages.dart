97fa10bfea35b824e7e4ea10b5db400b49e1c11f
Andres Araujo <a.araujo.azua@gmail.com>
2018-08-09 17:38:59 -0500

    wip v2

---
+++ timeago/lib/src/messages/es_messages.dart
@@ -0,0 +1,38 @@
+import 'package:timeago/src/messages/lookupmessages.dart';
+
+class EsMessages implements LookupMessages {
+  String prefixAgo() => 'hace';
+  String prefixFromNow() => 'dentro de';
+  String suffixAgo() => '';
+  String suffixFromNow() => '';
+  String lessThanOneMinute(int seconds) => 'un momento';
+  String aboutAMinute(int minutes) => 'un minuto';
+  String minutes(int minutes) => '$minutes minutos';
+  String aboutAnHour(int minutes) => 'una hora';
+  String hours(int hours) => '$hours horas';
+  String aDay(int hours) => 'un día';
+  String days(int days) => '$days dias';
+  String aboutAMonth(int days) => 'un mes';
+  String months(int months) => '$months meses';
+  String aboutAYear(int year) => 'un año';
+  String years(int years) => '$years años';
+  wordSeparator() => ' ';
+}
+class EsShortMessages implements LookupMessages {
+  String prefixAgo() => '';
+  String prefixFromNow() => '';
+  String suffixAgo() => '';
+  String suffixFromNow() => '';
+  String lessThanOneMinute(int seconds) => 'ahora';
+  String aboutAMinute(int minutes) => '1 min';
+  String minutes(int minutes) => '$minutes min';
+  String aboutAnHour(int minutes) => '~1 hr';
+  String hours(int hours) => '$hours hr';
+  String aDay(int hours) => '~1 día';
+  String days(int days) => '$days dias';
+  String aboutAMonth(int days) => '~1 mes';
+  String months(int months) => '$months meses';
+  String aboutAYear(int year) => '~1 año';
+  String years(int years) => '$years años';
+  wordSeparator() => ' ';
+}
---
+++ timeago/lib/src/messages/zh_messages.dart
@@ -0,0 +1,20 @@
+import 'package:timeago/src/messages/lookupmessages.dart';
+
+class ZhMessages implements LookupMessages {
+  String prefixAgo() => '';
+  String prefixFromNow() => '從現在開始';
+  String suffixAgo() => '前';
+  String suffixFromNow() => '後';
+  String lessThanOneMinute(int seconds) => '少於一分鐘';
+  String aboutAMinute(int minutes) => '約1分鐘前';
+  String minutes(int minutes) => '${minutes} 分';
+  String aboutAnHour(int minutes) => '約1小時';
+  String hours(int hours) => '約 ${hours} 小時';
+  String aDay(int hours) => '約1天';
+  String days(int days) => '約 ${days} 日';
+  String aboutAMonth(int days) => '約1個月';
+  String months(int months) => '約 ${months} 月';
+  String aboutAYear(int year) => '約1年';
+  String years(int years) => '約 ${years} 年';
+  wordSeparator() => ' ';
+}
