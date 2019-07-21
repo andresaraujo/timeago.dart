44115108a58796c532fe9fe64dafb1ad690744bd
Khoshbin Ali Ahmed <xoshbin@gmail.com>
2019-07-21 18:29:23 +0300

    Fixed RTL issue with numbers

--- timeago/lib/src/messages/ku_messages.dart
+++ timeago/lib/src/messages/ku_messages.dart
@@ -7,15 +7,15 @@ class KuMessages implements LookupMessages {
   String suffixFromNow() => 'لە ئێستاوە';
   String lessThanOneMinute(int seconds) => 'خولەکێک';
   String aboutAMinute(int minutes) => 'خولەکێک';
-  String minutes(int minutes) => '$minutes خولەک';
+  String minutes(int minutes) => 'خولەک $minutes';
   String aboutAnHour(int minutes) => 'نزیکەی کاژێرێک';
-  String hours(int hours) => '$hours کاژێر';
+  String hours(int hours) => 'کاژێر $hours ';
   String aDay(int hours) => 'ڕۆژێک';
-  String days(int days) => '$days ڕۆژ';
+  String days(int days) => 'ڕۆژ $days';
   String aboutAMonth(int days) => 'نزیکەی مانگێک';
-  String months(int months) => '$months مانگ';
+  String months(int months) => 'مانگ $months';
   String aboutAYear(int year) => 'نزیکەی ساڵێک';
-  String years(int years) => '$years ساڵ';
+  String years(int years) => 'ساڵ $years';
   String wordSeparator() => ' ';
 }
 
@@ -26,14 +26,14 @@ class KuShortMessages implements LookupMessages {
   String suffixFromNow() => '';
   String lessThanOneMinute(int seconds) => 'ئێستا';
   String aboutAMinute(int minutes) => '1 خولەک';
-  String minutes(int minutes) => '$minutes خولەک';
-  String aboutAnHour(int minutes) => '~1 کاژێر';
-  String hours(int hours) => '$hours کاژێر';
+  String minutes(int minutes) => 'خولەک $minutes';
+  String aboutAnHour(int minutes) => 'کاژێر ~1';
+  String hours(int hours) => 'کاژێر $hours';
   String aDay(int hours) => '~1 ڕۆژ';
-  String days(int days) => '$days رۆژ';
+  String days(int days) => 'رۆژ $days';
   String aboutAMonth(int days) => '~1 مانگ';
-  String months(int months) => '$months مانگ';
+  String months(int months) => 'مانگ $months';
   String aboutAYear(int year) => '~1 ساڵ';
-  String years(int years) => '$years ساڵ';
+  String years(int years) => 'ساڵ $years ';
   String wordSeparator() => ' ';
 }
