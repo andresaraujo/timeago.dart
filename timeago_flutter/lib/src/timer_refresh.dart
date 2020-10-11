16a4a9b02839fd9b3d239ab4c392003c24287ced
Frank Moreno <frankmoreno1993@gmail.com>
2020-10-11 10:18:33 -0500

    TimerRefresh documented

--- timeago_flutter/lib/src/timer_refresh.dart
+++ timeago_flutter/lib/src/timer_refresh.dart
@@ -1,16 +1,20 @@
 import 'timer_refresh_widget.dart';
 import 'package:flutter/widgets.dart';
 
+/// Refresh/rebuild the widget based on a [Duration]
 class TimerRefresh extends TimerRefreshWidget {
   const TimerRefresh({
     Key key,
     this.child,
     this.builder,
     Duration refreshRate,
-  })  : assert(builder != null && child != null),
+  })  : assert(builder != null),
         super(key: key, refreshRate: refreshRate);
 
+  /// Optional `child`
   final Widget child;
+
+  /// The `builder` has access to the `child` if necessary and rebuilds.
   final TransitionBuilder builder;
 
   @override
