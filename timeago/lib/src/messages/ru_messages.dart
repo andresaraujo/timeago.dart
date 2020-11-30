3d9770fc8c9f4edb2a9929e7bdef1219a447b75c
goolpe <gooolpe@gmail.com>
2020-11-30 14:36:35 +0300

    fix short version

--- timeago/lib/src/messages/ru_messages.dart
+++ timeago/lib/src/messages/ru_messages.dart
@@ -102,23 +102,23 @@ class RuShortMessages implements LookupMessages {
   @override
   String aboutAMinute(int minutes) => '1 мин.';
   @override
-  String minutes(int minutes) => 'мин.';
+  String minutes(int minutes) => '$minutes мин.';
   @override
   String aboutAnHour(int minutes) => '~1 ч.';
   @override
-  String hours(int hours) => 'ч.';
+  String hours(int hours) => '$hours ч.';
   @override
   String aDay(int hours) => '~1 д.';
   @override
-  String days(int days) => 'д.';
+  String days(int days) => '$days д.';
   @override
   String aboutAMonth(int days) => '~1 мес.';
   @override
-  String months(int months) => 'мес.';
+  String months(int months) => '$months мес.';
   @override
   String aboutAYear(int year) => '~1 г.';
   @override
-  String years(int years) => 'г.';
+  String years(int years) => '$years г.';
   @override
   String wordSeparator() => ' ';
 }
