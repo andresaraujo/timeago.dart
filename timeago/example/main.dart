8c1896df418c834e6a3ce82be5a97c89f2d236df
Andres Araujo <a.araujo.azua@gmail.com>
2021-06-10 16:32:49 -0500

    Add missing locales to web example

--- timeago/example/main.dart
+++ timeago/example/main.dart
@@ -8,81 +8,92 @@ final listContainer = querySelector("#list");
 var locale = 'en';
 
 main() async {
+  timeago.setLocaleMessages('ar', timeago.ArMessages());
+  timeago.setLocaleMessages('ar_short', timeago.ArShortMessages());
+  timeago.setLocaleMessages('az', timeago.AzMessages());
+  timeago.setLocaleMessages('az_short', timeago.AzShortMessages());
+  timeago.setLocaleMessages('ca', timeago.CaMessages());
+  timeago.setLocaleMessages('ca_short', timeago.CaShortMessages());
+  timeago.setLocaleMessages('cs', timeago.CsMessages());
+  timeago.setLocaleMessages('cs_short', timeago.CsShortMessages());
+  timeago.setLocaleMessages('da', timeago.DaMessages());
+  timeago.setLocaleMessages('da_short', timeago.DaShortMessages());
   timeago.setLocaleMessages('de', timeago.DeMessages());
+  timeago.setLocaleMessages('de_short', timeago.DeShortMessages());
   timeago.setLocaleMessages('dv', timeago.DvMessages());
   timeago.setLocaleMessages('dv_short', timeago.DvShortMessages());
+  timeago.setLocaleMessages('en', timeago.EnMessages());
+  timeago.setLocaleMessages('en_short', timeago.EnShortMessages());
+  timeago.setLocaleMessages('es', timeago.EsMessages());
+  timeago.setLocaleMessages('es_short', timeago.EsShortMessages());
+  timeago.setLocaleMessages('fa', timeago.FaMessages());
   timeago.setLocaleMessages('fr', timeago.FrMessages());
   timeago.setLocaleMessages('fr_short', timeago.FrShortMessages());
   timeago.setLocaleMessages('gr', timeago.GrMessages());
   timeago.setLocaleMessages('gr_short', timeago.GrShortMessages());
   timeago.setLocaleMessages('he', timeago.HeMessages());
   timeago.setLocaleMessages('he_short', timeago.HeShortMessages());
-  timeago.setLocaleMessages('ca', timeago.CaMessages());
-  timeago.setLocaleMessages('ca_short', timeago.CaShortMessages());
-  timeago.setLocaleMessages('cs', timeago.CsMessages());
-  timeago.setLocaleMessages('cs_short', timeago.CsShortMessages());
   timeago.setLocaleMessages('he', timeago.HeMessages());
   timeago.setLocaleMessages('he_short', timeago.HeShortMessages());
   timeago.setLocaleMessages('hi', timeago.HiMessages());
   timeago.setLocaleMessages('hi_short', timeago.HiShortMessages());
+  timeago.setLocaleMessages('id', timeago.IdMessages());
+  timeago.setLocaleMessages('it', timeago.ItMessages());
+  timeago.setLocaleMessages('it_short', timeago.ItShortMessages());
   timeago.setLocaleMessages('ja', timeago.JaMessages());
   timeago.setLocaleMessages('km', timeago.KmMessages());
   timeago.setLocaleMessages('km_short', timeago.KmShortMessages());
   timeago.setLocaleMessages('ko', timeago.KoMessages());
-  timeago.setLocaleMessages('id', timeago.IdMessages());
-  timeago.setLocaleMessages('pt_BR', timeago.PtBrMessages());
-  timeago.setLocaleMessages('pt_BR_short', timeago.PtBrShortMessages());
-  timeago.setLocaleMessages('zh_CN', timeago.ZhCnMessages());
-  timeago.setLocaleMessages('zh', timeago.ZhMessages());
-  timeago.setLocaleMessages('it', timeago.ItMessages());
-  timeago.setLocaleMessages('it_short', timeago.ItShortMessages());
-  timeago.setLocaleMessages('fa', timeago.FaMessages());
-  timeago.setLocaleMessages('tr', timeago.TrMessages());
-  timeago.setLocaleMessages('pl', timeago.PlMessages());
-  timeago.setLocaleMessages('th', timeago.ThMessages());
-  timeago.setLocaleMessages('th_short', timeago.ThShortMessages());
+  timeago.setLocaleMessages('ku', timeago.KuMessages());
+  timeago.setLocaleMessages('ku_short', timeago.KuShortMessages());
   timeago.setLocaleMessages('mn', timeago.MnMessages());
   timeago.setLocaleMessages('mn_short', timeago.MnShortMessages());
   timeago.setLocaleMessages('ms_MY', timeago.MsMyMessages());
   timeago.setLocaleMessages('ms_MY_short', timeago.MsMyShortMessages());
   timeago.setLocaleMessages('nb_NO', timeago.NbNoMessages());
   timeago.setLocaleMessages('nb_NO_short', timeago.NbNoShortMessages());
+  timeago.setLocaleMessages('nl', timeago.NlMessages());
+  timeago.setLocaleMessages('nl_short', timeago.NlShortMessages());
   timeago.setLocaleMessages('nn_NO', timeago.NnNoMessages());
   timeago.setLocaleMessages('nn_NO_short', timeago.NnNoShortMessages());
-  timeago.setLocaleMessages('ku', timeago.KuMessages());
-  timeago.setLocaleMessages('ku_short', timeago.KuShortMessages());
-  timeago.setLocaleMessages('ar', timeago.ArMessages());
-  timeago.setLocaleMessages('ar_short', timeago.ArShortMessages());
-  timeago.setLocaleMessages('ko', timeago.KoMessages());
-  timeago.setLocaleMessages('vi', timeago.ViMessages());
-  timeago.setLocaleMessages('vi_short', timeago.ViShortMessages());
-  timeago.setLocaleMessages('tr', timeago.TrMessages());
-  timeago.setLocaleMessages('ta', timeago.TaMessages());
+  timeago.setLocaleMessages('pl', timeago.PlMessages());
+  timeago.setLocaleMessages('pt_BR', timeago.PtBrMessages());
+  timeago.setLocaleMessages('pt_BR_short', timeago.PtBrShortMessages());
   timeago.setLocaleMessages('ro', timeago.RoMessages());
   timeago.setLocaleMessages('ro_short', timeago.RoShortMessages());
   timeago.setLocaleMessages('ru', timeago.RuMessages());
   timeago.setLocaleMessages('ru_short', timeago.RuShortMessages());
+  timeago.setLocaleMessages('rw', timeago.RwMessages());
+  timeago.setLocaleMessages('rw_short', timeago.RwShortMessages());
   timeago.setLocaleMessages('sv', timeago.SvMessages());
   timeago.setLocaleMessages('sv_short', timeago.SvShortMessages());
+  timeago.setLocaleMessages('ta', timeago.TaMessages());
+  timeago.setLocaleMessages('th', timeago.ThMessages());
+  timeago.setLocaleMessages('th_short', timeago.ThShortMessages());
+  timeago.setLocaleMessages('tr', timeago.TrMessages());
   timeago.setLocaleMessages('uk', timeago.UkMessages());
   timeago.setLocaleMessages('uk_short', timeago.UkShortMessages());
+  timeago.setLocaleMessages('vi', timeago.ViMessages());
+  timeago.setLocaleMessages('vi_short', timeago.ViShortMessages());
+  timeago.setLocaleMessages('zh_CN', timeago.ZhCnMessages());
+  timeago.setLocaleMessages('zh', timeago.ZhMessages());
 
   final loadedTime = new DateTime.now();
 
   final updateMainContainer = () {
     final now = new DateTime.now();
     final difference = now.difference(loadedTime);
-    mainContainer.text =
+    mainContainer?.text =
         timeago.format(now.subtract(difference), locale: locale);
   };
 
   querySelectorAll(".locale-link").onClick.listen((event) async {
     final el = event.target as AnchorElement;
 
-    locale = el.text;
+    locale = el.text ?? 'en';
 
     // Recreate list items
-    listContainer.innerHtml = "";
+    listContainer?.innerHtml = "";
     createListItems();
   });
 
@@ -93,7 +104,7 @@ main() async {
 }
 
 addItem(String text) {
-  listContainer.append(new LIElement()..text = text);
+  listContainer?.append(new LIElement()..text = text);
 }
 
 createListItems() {
