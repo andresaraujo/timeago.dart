b05dfc80a002d03730ae26cca5cc5913ad4e7d79
Alto <altotunchitoo@icloud.com>
2022-04-30 23:17:27 +0630

    Updated messages for Myanmar language

--- packages/timeago/lib/src/messages/my_messages.dart
+++ packages/timeago/lib/src/messages/my_messages.dart
@@ -5,7 +5,7 @@ import 'package:timeago/src/messages/lookupmessages.dart';
 /// မြန်မာစာ
 class MyMessages implements LookupMessages {
   @override
-  String prefixAgo() => 'လွန်ခဲ့သော ';
+  String prefixAgo() => '';
 
   @override
   String prefixFromNow() => 'ယခုမှစ၍ ';
@@ -20,40 +20,92 @@ class MyMessages implements LookupMessages {
   String lessThanOneMinute(int seconds) => 'စောနက';
 
   @override
-  String aboutAMinute(int minutes) => '၁ မိနစ်';
+  String aboutAMinute(int minutes) => 'လွန်ခဲ့သော ၁ မိနစ်ခန့်';
 
   @override
   String minutes(int minutes) =>
-      '${NumberFormat.compact(locale: 'my').format(minutes)} မိနစ်';
+      'လွန်ခဲ့သော ${NumberFormat.compact(locale: 'my').format(minutes)} မိနစ်';
 
   @override
-  String aboutAnHour(int minutes) => '၁ နာရီ';
+  String aboutAnHour(int minutes) => 'လွန်ခဲ့သော ၁ နာရီခန့်';
 
   @override
   String hours(int hours) =>
-      '${NumberFormat.compact(locale: 'my').format(hours)} နာရီ';
+      'လွန်ခဲ့သော ${NumberFormat.compact(locale: 'my').format(hours)} နာရီ';
 
   @override
-  String aDay(int hours) => '၁ ရက်';
+  String aDay(int hours) => 'လွန်ခဲ့သော ၁ ရက်';
 
   @override
   String days(int days) =>
-      '${NumberFormat.compact(locale: 'my').format(days)} ရက်';
+      'လွန်ခဲ့သော ${NumberFormat.compact(locale: 'my').format(days)} ရက်';
 
   @override
-  String aboutAMonth(int days) => '၁ လ';
+  String aboutAMonth(int days) => 'လွန်ခဲ့သော ၁ လခန့်';
 
   @override
   String months(int months) =>
-      '${NumberFormat.compact(locale: 'my').format(months)} လ';
+      'လွန်ခဲ့သော ${NumberFormat.compact(locale: 'my').format(months)} လ';
 
   @override
-  String aboutAYear(int year) => '၁ နှစ်';
+  String aboutAYear(int year) => 'လွန်ခဲ့သော ၁ နှစ်ခန့်';
 
   @override
   String years(int years) =>
-      '${NumberFormat.compact(locale: 'my').format(years)} နှစ်';
+      'လွန်ခဲ့သော ${NumberFormat.compact(locale: 'my').format(years)} နှစ်';
 
+  @override
+  String wordSeparator() => ' ';
+}
+
+/// Myanmar short Messages
+class MyShortMessages implements LookupMessages {
+  @override
+  String prefixAgo() => '';
+  @override
+  String prefixFromNow() => '';
+  @override
+  String suffixAgo() => '';
+  @override
+  String suffixFromNow() => '';
+
+  @override
+  String lessThanOneMinute(int seconds) => 'စောနက';
+
+  @override
+  String aboutAMinute(int minutes) => '၁မိနစ်';
+
+  @override
+  String minutes(int minutes) =>
+      '${NumberFormat.compact(locale: 'my').format(minutes)}မိနစ်';
+
+  @override
+  String aboutAnHour(int minutes) => '၁နာရီ';
+
+  @override
+  String hours(int hours) =>
+      '${NumberFormat.compact(locale: 'my').format(hours)}နာရီ';
+
+  @override
+  String aDay(int hours) => '၁ရက်';
+
+  @override
+  String days(int days) =>
+      '${NumberFormat.compact(locale: 'my').format(days)}ရက်';
+
+  @override
+  String aboutAMonth(int days) => '၁လ';
+
+  @override
+  String months(int months) =>
+      '${NumberFormat.compact(locale: 'my').format(months)}လ';
+
+  @override
+  String aboutAYear(int year) => '၁နှစ်';
+
+  @override
+  String years(int years) =>
+      '${NumberFormat.compact(locale: 'my').format(years)}နှစ်';
   @override
   String wordSeparator() => ' ';
 }
